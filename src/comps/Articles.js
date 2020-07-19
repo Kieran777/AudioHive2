43cf9b06bf6d99c284973b57ee31dd4020682a26
Philip Koekemoer <philips.progdev@gmail.com>
2020-07-19 17:04:37 +0200

    second commit

---
+++ src/comps/Articles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Articles.css';
+
+function Articles(){
+    
+        return(
+            <div id="mainInsert">
+                <h2 className="highlightHeadings">Articles</h2>
+            </div>
+        )
+    
+}
+
+export default Articles;
---
+++ src/comps/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact(){
+    
+        return(
+            <div id="mainInsert">
+                <h2 className="highlightHeadings">Contact Us</h2>
+                <h3 className="highlightHeadings">To Contact us:</h3>
+                <h3>Send us an Email at:  </h3>
+                <a href="mailto: Audiohiveproject@gmail.com">Audiohiveproject@gmail.com</a>
+            </div>
+        )
+    
+}
+
+export default Contact;
