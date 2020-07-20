42c88f22fb7b4a3317d0f04752e7da6c01ef954c
Philip Koekemoer <philips.progdev@gmail.com>
2020-07-20 07:47:22 +0200

    Updates

---
+++ precache-manifest.36b6984b666724c0f049755ea299b4c8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b244249eb0eae7386000a363dd2992c2",
+    "url": "/AudioHive/index.html"
+  },
+  {
+    "revision": "ce1cea98fdf2973b6999",
+    "url": "/AudioHive/static/css/main.74d13952.chunk.css"
+  },
+  {
+    "revision": "4f329eb7c3d859143846",
+    "url": "/AudioHive/static/js/2.14f6a8c9.chunk.js"
+  },
+  {
+    "revision": "a531f1e27e5cd8d76c40d53140628768",
+    "url": "/AudioHive/static/js/2.14f6a8c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1cea98fdf2973b6999",
+    "url": "/AudioHive/static/js/main.6be8b9d8.chunk.js"
+  },
+  {
+    "revision": "c55939768632072a52ad",
+    "url": "/AudioHive/static/js/runtime-main.dc2aa819.js"
+  },
+  {
+    "revision": "096ca1302d252d8d3e55dbf23b0e84ab",
+    "url": "/AudioHive/static/media/audiohiveLogo.096ca130.png"
+  },
+  {
+    "revision": "22c534473cd70074ef7ca3de25db3269",
+    "url": "/AudioHive/static/media/audiohiveName.22c53447.png"
+  },
+  {
+    "revision": "fc7cce1fabc0aea2eee9e84516b7f36f",
+    "url": "/AudioHive/static/media/bees.fc7cce1f.png"
+  },
+  {
+    "revision": "a0b46d84e8b961b71388065bfe16b39c",
+    "url": "/AudioHive/static/media/caoImg.a0b46d84.png"
+  },
+  {
+    "revision": "831fe68c033167dad07c24a2e91c6ec4",
+    "url": "/AudioHive/static/media/cdoImg.831fe68c.png"
+  },
+  {
+    "revision": "1a7e2c30158290285905444235720bd2",
+    "url": "/AudioHive/static/media/ceoImg.1a7e2c30.png"
+  },
+  {
+    "revision": "303c50b1c961bc22da37b5b6d4d97962",
+    "url": "/AudioHive/static/media/cioImg.303c50b1.png"
+  },
+  {
+    "revision": "4c294052758913ec842425a25526f94c",
+    "url": "/AudioHive/static/media/cvoImg.4c294052.png"
+  },
+  {
+    "revision": "a7c2d153760c636e1dff9fb2e170201f",
+    "url": "/AudioHive/static/media/hrImg.a7c2d153.png"
+  },
+  {
+    "revision": "0e2092a9af7e7333f14cff74bd5c2639",
+    "url": "/AudioHive/static/media/internImg.0e2092a9.png"
+  },
+  {
+    "revision": "99d8a3aef36ee0df2dea5d337ca4b7f1",
+    "url": "/AudioHive/static/media/mmImg.99d8a3ae.png"
+  },
+  {
+    "revision": "66922522c45ed17739962f84a2c8e92f",
+    "url": "/AudioHive/static/media/workImg1.66922522.png"
+  },
+  {
+    "revision": "2eb856f155f7c30be3cc03436a94acfe",
+    "url": "/AudioHive/static/media/workImg2.2eb856f1.png"
+  }
+]);
